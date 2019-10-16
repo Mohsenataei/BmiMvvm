8ff909c9961f2100a0c112d9596060f566850ad5
mohsen <mohsenataeiasad@gmail.com>
2019-10-16 16:51:17 +0330

    bmi activity added

---
+++ app/src/main/java/com/mohsen/caculatebmi_mvvm/ui/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.mohsen.caculatebmi_mvvm.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohsen.caculatebmi_mvvm.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
