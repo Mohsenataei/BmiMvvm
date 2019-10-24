b80edb23818e71100858395cd9b1006d11bf62a3
mohsen <mohsenataeiasad@gmail.com>
2019-10-24 14:24:50 +0330

    add some exercise to list.

--- app/src/main/java/com/mohsen/caculatebmi_mvvm/data/ExercisesExpandableListDataPump.kt
+++ app/src/main/java/com/mohsen/caculatebmi_mvvm/data/ExercisesExpandableListDataPump.kt
@@ -16,6 +16,7 @@ class ExercisesExpandableListDataPump {
         val solo = ArrayList<String>()
         val swimming = ArrayList<String>()
         val others = ArrayList<String>()
+        val means = ArrayList<String>()
 
 
 
@@ -26,6 +27,8 @@ class ExercisesExpandableListDataPump {
         walking.add("تپه نوردی بدون بار")
         walking.add("راه رفتن آهسته (کمتر از ۳ کیلو متر در ساعت)")
         walking.add("راه رفتن آهسته در آب")
+        walking.add("ورزش های چرخشی شامل چندین حرکت هوازی با کمترین استراحت")
+        walking.add(" آهسته دویدن و راه رفتن به صورت ترکیبی")
 
         walking.add("راه رفتن با سرعت متوسط در آب")
         walking.add("راه رفتن روی سطح صاف ( ۳ کیلو متر در ساعت)")
@@ -33,14 +36,35 @@ class ExercisesExpandableListDataPump {
         walking.add("راه رفتن خیلی سریع روی سطح صاف ( ۷ کیلو متر در ساعت)")
         walking.add("راه رفتن روی چمن")
         walking.add("راه رفتن روی شن")
-        walking.add("بالارفتن از پله ها با سرعت بالا")
-        walking.add("بالارفتن از پله ها با سرعت بالا")
-        walking.add("بالارفتن از پله ها با سرعت بالا")
-        walking.add("بالارفتن از پله ها با سرعت بالا")
-        walking.add("بالارفتن از پله ها با سرعت بالا")
-        walking.add("بالارفتن از پله ها با سرعت بالا")
-        walking.add("بالارفتن از پله ها با سرعت بالا")
-        walking.add("بالارفتن از پله ها با سرعت بالا")
+        walking.add(" پیاده روی با سرعت کم روی سطح صاف")
+        walking.add("پیاده روی با سرعت متوسط روی سطح صاف")
+        walking.add(" پیاده روی با سرعت متوسط در سربالایی یا بالا رفتن از تپه")
+        walking.add("پیاده روی با سرعت بالا روی سطح صاف")
+        walking.add("پیاده روی با سرعت بسیار بالا روی سطح صاف")
+        walking.add("پیاده روی تا محل کار با سرعت متوسط بدون حمل چیزی")
+        walking.add("پیاده روی تا محل کار با سرعت زیاد بدون حمل چیزی")
+        walking.add("پیاده روی تا محل کار با سرعت کم بدون حمل چیزی")
+        walking.add("پایین رفتن از پله و حمل جسمی با وزن 45 کیلو یا بیشتر")
+        walking.add("پایین رفتن از پله و حمل جسمی با وزن 11 تا 22 کیلو")
+        walking.add("پایین رفتن از پله و حمل جسمی با وزن 23 تا 33 کیلو")
+        walking.add("پایین رفتن از پله و حمل جسمی با وزن 34 تا 45 کیلو")
+        walking.add("پیاده روی با سرعت کم و حمل جسمی با وزن کمتر از 11 کیلو")
+        walking.add(" پیاده روی با سرعت متوسط و حمل جسمی با وزن کمتر از 11 کیلو")
+        walking.add("پیاده روی با سرعت بالا و حمل جسمی با وزن کمتر از 11 کیلو")
+        walking.add("راه رفتن و هل دادن صندلی چرخ دار")
+        walking.add("پیاده روی در جاهای ناهموار")
+        walking.add("بالا رفتن از صخره یا کوه")
+        walking.add("راه رفتن با استفاده از عصا")
+        walking.add("دویدن با سرعت بسیار بالا")
+        walking.add("دویدن با سرعت بالا")
+        walking.add("دویدن با شدت متوسط")
+        walking.add("دویدن با شدت کم")
+        walking.add("دویدن با سرعت بسیار کم")
+        walking.add("طناب زدن، با سرعت بالا")
+        walking.add("راه رفتن با کودک")
+        walking.add("طناب زدن با سرعت متوسط")
+        walking.add("طناب زدن با سرعت کم")
+        walking.add(" راه رفتن با کوله پشتی")
 
         //init running
 
@@ -145,6 +169,17 @@ class ExercisesExpandableListDataPump {
         others.add("بولینگ")
         others.add("بیلیارد")
         others.add("پیلانس")
+
+        means.add("دوچرخه ثابت، 100 وات، شدت کم)")
+        means.add("دوچرخه ثابت، 150 وات، شدت معمولی)")
+        means.add("دوچرخه ثابت، 200 وات، شدت بالا)")
+        means.add("دوچرخه ثابت، 250 وات، شدت بسیار بالا)")
+        means.add("دوچرخه ثابت، 50 وات، شدت بسیار کم)")
+        means.add("قایقرانی ثابت، 100 وات، شدت متوسط")
+        means.add("قایقرانی ثابت، 150 وات، شدت بالا")
+        means.add("قایقرانی ثابت، 200 وات، شدت بسیار بالا")
+        means.add("قایقرانی ثابت، 50 وات، شدت بسیار کم")
+        means.add("ماشین اسکی معمولی")
 
         expandableListDetail["پیاده روی"] = walking
         expandableListDetail["دویدن"] = running
