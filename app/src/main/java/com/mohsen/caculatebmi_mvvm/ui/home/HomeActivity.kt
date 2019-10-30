bc41cfc24ecc051a525ef39f706acb3bdfa17c85
mohsen <mohsenataeiasad@gmail.com>
2019-10-30 16:59:45 +0330

    add some icons.

--- app/src/main/java/com/mohsen/caculatebmi_mvvm/ui/home/HomeActivity.kt
+++ app/src/main/java/com/mohsen/caculatebmi_mvvm/ui/home/HomeActivity.kt
@@ -7,6 +7,13 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.anychart.AnyChart
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.enums.Anchor
+import com.anychart.enums.HoverMode
+import com.anychart.enums.Position
+import com.anychart.enums.TooltipPositionMode
 
 
 import com.mohsen.caculatebmi_mvvm.adapters.RecyclerViewAdapter
@@ -42,43 +49,43 @@ class HomeActivity : AppCompatActivity() {
         loadStoredData()
         addExtraToList()
 
-//        val cartesian = AnyChart.column()
-//
-//        val data = ArrayList<DataEntry>()
-//        data.add(ValueDataEntry("Rouge", 80540))
-//        data.add(ValueDataEntry("Foundation", 94190))
-//        data.add(ValueDataEntry("Mascara", 102610))
-//        data.add(ValueDataEntry("Lip gloss", 110430))
-//        data.add(ValueDataEntry("Lipstick", 128000))
-//        data.add(ValueDataEntry("Nail polish", 143760))
-//        data.add(ValueDataEntry("Eyebrow pencil", 170670))
-//        data.add(ValueDataEntry("Eyeliner", 213210))
-//        data.add(ValueDataEntry("Eyeshadows", 249980))
-//
-//        val column = cartesian.column(data)
-//
-//        column.tooltip()
-//            .titleFormat("{%X}")
-//            .position(Position.CENTER_BOTTOM)
-//            .anchor(Anchor.CENTER_BOTTOM)
-//            .offsetX(0.0)
-//            .offsetY(5.0)
-//            .format("\${%Value}{groupsSeparator: }")
-//
-//        cartesian.animation(true)
-//        cartesian.title("Top 10 Cosmetic Products by Revenue")
-//
-//        cartesian.yScale().minimum(0.0)
-//
-//        cartesian.yAxis(0).labels().format("\${%Value}{groupsSeparator: }")
-//
-//        cartesian.tooltip().positionMode(TooltipPositionMode.POINT)
-//        cartesian.interactivity().hoverMode(HoverMode.BY_X)
-//
-//        cartesian.xAxis(0).title("Product")
-//        cartesian.yAxis(0).title("Revenue")
-//
-//        homePageChart.setChart(cartesian)
+        val cartesian = AnyChart.column()
+
+        val data = ArrayList<DataEntry>()
+        data.add(ValueDataEntry("Rouge", 80540))
+        data.add(ValueDataEntry("Foundation", 94190))
+        data.add(ValueDataEntry("Mascara", 102610))
+        data.add(ValueDataEntry("Lip gloss", 110430))
+        data.add(ValueDataEntry("Lipstick", 128000))
+        data.add(ValueDataEntry("Nail polish", 143760))
+        data.add(ValueDataEntry("Eyebrow pencil", 170670))
+        data.add(ValueDataEntry("Eyeliner", 213210))
+        data.add(ValueDataEntry("Eyeshadows", 249980))
+
+        val column = cartesian.column(data)
+
+        column.tooltip()
+            .titleFormat("{%X}")
+            .position(Position.CENTER_BOTTOM)
+            .anchor(Anchor.CENTER_BOTTOM)
+            .offsetX(0.0)
+            .offsetY(5.0)
+            .format("\${%Value}{groupsSeparator: }")
+
+        cartesian.animation(true)
+        cartesian.title("Top 10 Cosmetic Products by Revenue")
+
+        cartesian.yScale().minimum(0.0)
+
+        cartesian.yAxis(0).labels().format("\${%Value}{groupsSeparator: }")
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT)
+        cartesian.interactivity().hoverMode(HoverMode.BY_X)
+
+        cartesian.xAxis(0).title("Product")
+        cartesian.yAxis(0).title("Revenue")
+
+        homePageChart.setChart(cartesian)
         //saveOnExit()
 
 //        if (commonList.isEmpty()){
